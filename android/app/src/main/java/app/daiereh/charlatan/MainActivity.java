9a7fb040d65dffbe86a64c0b56b3216aa6dbacd1
Alireza Hadjar <asiahadjar@gmail.com>
2021-07-11 17:42:37 +0430

    change name to charlatan

--- android/app/src/main/java/app/daiereh/charlatan/MainActivity.java
+++ android/app/src/main/java/app/daiereh/charlatan/MainActivity.java
@@ -1,4 +1,4 @@
-package com.spyhunt;
+package app.daiereh.charlatan;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SpyHunt";
+    return "Charlatan";
   }
 }
