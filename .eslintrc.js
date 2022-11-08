b19d8d5a9003fc3896cfb2b42a3a4a34833709f2
Alireza Hadjar <asiahadjar@gmail.com>
2022-11-08 12:07:50 +0330

    upgraded rn version to v0.71-rc-1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     root: true,
     extends: ["@react-native-community", "react-native-wcandillon"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
     rules: {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-shadow": "off",
