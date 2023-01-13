2e1b347ff0949744e5b0bbdd8e030d02f147fad1
Alireza Hadjar <asiahadjar@gmail.com>
2023-01-13 22:57:53 +0330

    updated to v0.71.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
     root: true,
     extends: ["@react-native-community", "react-native-wcandillon"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
     rules: {
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-shadow": "off",
