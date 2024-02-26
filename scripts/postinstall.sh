2f2eeb492644805e495ea50ecad150ca000412ed
Alireza Hadjar <asiahadjar@gmail.com>
2024-02-26 19:56:04 +0330

    feat: added Yarn v4

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 # Specify the required global NPM packages.
-GLOBAL_NPM_SCRIPTS=( "patch-package" )
+GLOBAL_NPM_SCRIPTS=( "npx patch-package" )
 
 # Apply patches.
-patch-package
+npx patch-package
 echo "✅ All patches applied"
